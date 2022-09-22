e15ce4ac64ee182e8beec8f44ba6ae5f92eb4090
marcusdinh <marcus.dinh@envision-digital.com>
2022-09-22 17:11:33 +0700

    udpate

---
+++ CafeineCache/src/main/java/com/example/cafeinecache/CafeineCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafeinecache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeineCacheApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CafeineCacheApplication.class, args);
+  }
+
+}
---
+++ CafeineCache/src/test/java/com/example/cafeinecache/CafeineCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafeinecache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeineCacheApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
