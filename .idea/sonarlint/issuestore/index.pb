f1912d1e6eae10d720c7535d6928a30237e817ef
marcusdinh <marcus.dinh@envision-digital.com>
2022-12-08 17:40:56 +0700

    init spring mvc

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+‡
+Wredis-practice/src/main/java/com/chap6/redispractice/repository/AircraftRepository.java,9\5\95f26a1d65096360dcb794916be18fd2651599ee
+‚
+Rredis-practice/src/main/java/com/chap6/redispractice/RedisPracticeApplication.java,2\6\2639653dc6513de12974a77ce6929c8a1576ecfc
+†
+Vredis-practice/src/main/java/com/chap6/redispractice/controller/PlaneFinderPoller.java,3\1\31221d443dd1338d901ca75078c66e3bc1a746cb
+>
+no-sql/HELP.md,f\a\fa875e9fcaf069f0552da2f4c247beb1b503fd35
+v
+Fredis-practice/src/main/java/com/chap6/redispractice/dto/Aircraft.java,5\5\551d612cc5751585fae32aa46f10037b1e5e04ea
---
+++ no-sql/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+    id("org.springframework.boot") version "2.7.7-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
